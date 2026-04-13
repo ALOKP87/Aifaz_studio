1815ef45c2f96f0219f98887fbd41076f434cb6a
ALOKP87 <alokpandit675@gmail.com>
2026-04-13 13:41:57 +0530

    Create guard.js

---
+++ guard.js
@@ -0,0 +1,21 @@
+import { app } from "./firebase-init.js";
+
+import {
+  getFirestore,
+  doc,
+  getDoc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export async function checkAccess(){
+  const user = auth.currentUser;
+  if(!user) return false;
+
+  const snap = await getDoc(doc(db,"usersauth",user.uid));
+
+  return snap.exists();
+}
